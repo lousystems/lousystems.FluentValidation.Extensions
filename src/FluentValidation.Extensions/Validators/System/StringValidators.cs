55eeddde38ecddc56efac5ac70c336b5d3721ca2
Louis <81430377+lousystems@users.noreply.github.com>
2025-12-21 17:56:12 +0100

    Add initial implementation of StringValidators and corresponding tests

---
+++ src/FluentValidation.Extensions/Validators/System/StringValidators.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentValidation.Extensions.Validators.System;
+
+public static class StringValidators
+{
+    extension<T>(IRuleBuilder<T, string?> ruleBuilder)
+    {
+       
+    }
+}
---
+++ tests/FluentValidation.Extensions.Tests/Validators/System/StringValidatorTests.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentValidation.Extensions.Tests.Validators.System;
+
+public class StringValidatorTests
+{
+    private class TestModel
+    {
+        public string? Text { get; set; }
+    }
+
+    // ==========================================
+    // 1. Valid Cases
+    // ==========================================
+
+   
+}
